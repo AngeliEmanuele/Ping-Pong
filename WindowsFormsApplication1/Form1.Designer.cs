07876b79c3259cab707dc9b50363c0eca3188cc5
AngeliEmanuele <emanuele.angeli95@live.com>
2015-01-10 12:48:38 +0100

    Importata prima versione

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlGioco = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlRight = new System.Windows.Forms.Panel();
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PnlBall = new System.Windows.Forms.Panel();
+            this.pnlGioco.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnlGioco
+            // 
+            this.pnlGioco.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlGioco.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pnlGioco.Controls.Add(this.label2);
+            this.pnlGioco.Controls.Add(this.label1);
+            this.pnlGioco.Controls.Add(this.pnlRight);
+            this.pnlGioco.Controls.Add(this.pnlLeft);
+            this.pnlGioco.Controls.Add(this.button1);
+            this.pnlGioco.Controls.Add(this.PnlBall);
+            this.pnlGioco.Location = new System.Drawing.Point(0, 0);
+            this.pnlGioco.Name = "pnlGioco";
+            this.pnlGioco.Size = new System.Drawing.Size(464, 359);
+            this.pnlGioco.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(316, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(92, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "0";
+            // 
+            // pnlRight
+            // 
+            this.pnlRight.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pnlRight.Location = new System.Drawing.Point(433, 124);
+            this.pnlRight.Name = "pnlRight";
+            this.pnlRight.Size = new System.Drawing.Size(17, 100);
+            this.pnlRight.TabIndex = 6;
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pnlLeft.Location = new System.Drawing.Point(12, 124);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(17, 100);
+            this.pnlLeft.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "on/off";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PnlBall
+            // 
+            this.PnlBall.BackColor = System.Drawing.Color.Aqua;
+            this.PnlBall.Location = new System.Drawing.Point(220, 124);
+            this.PnlBall.Name = "PnlBall";
+            this.PnlBall.Size = new System.Drawing.Size(25, 26);
+            this.PnlBall.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(462, 356);
+            this.Controls.Add(this.pnlGioco);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.pnlGioco.ResumeLayout(false);
+            this.pnlGioco.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel pnlGioco;
+        private System.Windows.Forms.Panel pnlLeft;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel PnlBall;
+        private System.Windows.Forms.Panel pnlRight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double speed;
+        double angle;
+        double leftReal;
+        double topReal;
+        int inc;
+        int punti1, punti2;
+        
+    
+        public Form1()
+        {
+            InitializeComponent();
+            leftReal = PnlBall.Left;
+            topReal= PnlBall.Top;
+            speed = 5;
+            angle = 180;
+            inc = 5;
+            punti1 = 0;
+            punti2 = 0;
+            label1.Text = punti1.ToString();
+            label2.Text = punti2.ToString();
+
+        }
+                
+        
+        
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+      
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double pz = pnlLeft.Height / 5;
+            double rz = pnlRight.Height / 5;
+            
+            bool punto = false;
+            
+            double radianti = Math.PI * angle / 180.0;
+
+            double l = leftReal + speed * Math.Cos(radianti);
+            double t = topReal + speed * Math.Sin(radianti);
+            double b = t + PnlBall.Height;
+            
+            
+            // x = pnlRight.Left
+            int x = pnlRight.Left;
+
+            // y = pnlRight.Top 
+            int y = pnlRight.Top;
+
+            // a = pnlLeft.Left 
+            int a = pnlLeft.Left;
+
+            // c = pnlLeft.Top
+            int c = pnlLeft.Top;
+
+            if (((l + PnlBall.Width) >= x) &&
+               ((t + PnlBall.Height) >= y) &&
+               (t <= pnlRight.Bottom)
+                )
+                {
+                if ((t <= (pnlRight.Top + 3 * rz)) && (b >= (pnlRight.Top + 2 * rz)))
+                    angle = 180 - angle;
+                else if ((t <= (pnlRight.Top + 2 * rz)) && (b >= (pnlRight.Top + 1 * rz)))
+                    angle = 180 - angle - 2;
+                else if ((t <= (pnlRight.Top + 4 * rz)) && (b >= (pnlRight.Top + 3 * rz)))
+                    angle = 180 - angle + 2;
+                else if ((t <= (pnlRight.Top + 1 * rz)) && (b >= (pnlRight.Top + 0 * rz)))
+                    angle = 180 - angle - 5;
+                else if ((t <= (pnlRight.Top + 5 * rz)) && (b >= (pnlRight.Top + 4 * rz)))
+                    angle = 180 - angle + 5;
+                else
+                    angle = 180 - angle;
+                }
+
+            else if ((l <= pnlLeft.Right) &&
+                    ((t + PnlBall.Height) >= c) &&
+                    (t <= pnlLeft.Bottom)
+                     )
+                    {
+                        if ((t <= (pnlLeft.Top + 3*pz)) && (b >= (pnlLeft.Top + 2*pz)))
+                            angle = 180 - angle;
+                        else if ((t <= (pnlLeft.Top + 2*pz)) && (b >= (pnlLeft.Top + 1*pz)))
+                            angle = 180 - angle -2;
+                        else if ((t <= (pnlLeft.Top + 4*pz)) && (b >= (pnlLeft.Top + 3*pz)))
+                            angle = 180 - angle + 2;
+                        else if ((t <= (pnlLeft.Top + 1*pz)) && (b >= (pnlLeft.Top + 0*pz)))
+                            angle = 180 - angle -5;
+                        else if ((t <= (pnlLeft.Top + 5*pz)) && (b >= (pnlLeft.Top + 4*pz)))
+                            angle = 180 - angle +5;
+                        else
+                            angle = 180 - angle;
+                    }
+
+
+            else if (l <= 0)
+            {
+                punti2++;
+                punto = true;
+            }
+            else if ((l + PnlBall.Width) >= pnlGioco.Width)
+            {
+                punti1++;
+                punto = true;
+            }
+
+
+            else if ((t <= 0) ||
+                    ((t + PnlBall.Height) >= pnlGioco.Height)
+                    )
+                angle = 360 - angle;
+
+            if (angle < 0)
+                angle = 360 + angle;
+
+            radianti = Math.PI * angle / 180.0;
+           
+            leftReal = leftReal + speed * Math.Cos(radianti);
+            topReal = topReal + speed * Math.Sin(radianti);
+
+            PnlBall.Left = (int)Math.Round(leftReal);
+            PnlBall.Top = (int)Math.Round(topReal);
+
+
+            if (punto)
+            {
+                timer1.Enabled = false;
+                PnlBall.Visible = false;
+                label1.Text = punti1.ToString();
+                label2.Text = punti2.ToString();
+                PnlBall.Left = (pnlGioco.Width / 2) - (PnlBall.Width / 2);
+                PnlBall.Top = (pnlGioco.Height / 2) - (PnlBall.Height / 2);
+                leftReal = PnlBall.Left;
+                topReal = PnlBall.Top;
+                pnlLeft.Top = (pnlGioco.Height / 2) - (pnlLeft.Height / 2);
+                pnlRight.Top = (pnlGioco.Height / 2) - (pnlLeft.Height / 2);
+                speed = 3;
+                angle = -angle;
+                PnlBall.Visible = true;
+                timer1.Enabled = true;
+
+            }
+         }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.P)
+                pnlRight.Top = pnlRight.Top - inc;
+            if (e.KeyCode == Keys.L)
+                pnlRight.Top = pnlRight.Top + inc;
+
+            if (e.KeyCode == Keys.A)
+                pnlLeft.Top = pnlLeft.Top - inc;
+            if (e.KeyCode == Keys.Z)
+                pnlLeft.Top = pnlLeft.Top + inc;
+        }       
+    }
+  }
+
