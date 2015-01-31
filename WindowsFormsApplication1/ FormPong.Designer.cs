b992689f005091960b43fbfa6a92a7b0d346a8be
lorenzobusin <xbusox@gmail.com>
2015-01-31 12:42:29 +0100

    Update  FormPong.Designer.cs
    Inizializzazione opzioni grafiche

--- WindowsFormsApplication1/ FormPong.Designer.cs
+++ WindowsFormsApplication1/ FormPong.Designer.cs
@@ -1,1 +1,16 @@
 namespace Pong
+{
+    partial class frmPong
+    {
+      private System.ComponentModel.IContainer components = null;
+ 
+        protected override void Dispose(bool disposing)
+        {
+          if (disposing && (components != null))
+          {
+              components.Dispose();
+          }
+        base.Dispose(disposing);
+        }
+    }
+}
