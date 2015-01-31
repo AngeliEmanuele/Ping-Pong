0ede6e06d9be9c949c562208b2b8333532731477
AngeliEmanuele <emanuele.angeli95@live.com>
2015-01-31 12:31:59 +0100

    Update FormPong.cs
    Aggiunta delle variabili con relativi commenti per l'inizializzazione del gioco

--- WindowsFormsApplication1/FormPong.cs
+++ WindowsFormsApplication1/FormPong.cs
@@ -7,3 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+namespace Pong
+ {
+  public partial class frmPong : Form
+  {
+  double speed; // velocità della pallina
+  double angle; // angolo (in gradi) traiettoria della pallina
+  double leftReal; // dove è la palllina veramente
+  double topReal;
+ 
+  int incPadle; // incremento racchetta
+ 
+  int puntiOne, puntiTwo;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  }
+ }
